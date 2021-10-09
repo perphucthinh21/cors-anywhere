1e6136bf86c80dcabf0341f3c505f4bcf583fd53
perthinh <perthinh@gmail.com>
2021-10-09 10:41:28 +0700

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'https://portfolio-ravenous.herokuapp.com', 'http://18.191.112.47', 'http://spm.cnexco.com', ''],
+    originWhitelist: ['http://localhost:3000', 'https://portfolio-ravenous.herokuapp.com', 'http://18.191.112.47', 'http://spm.cnexco.com', 'https://spm.cnexco.com', ''],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
