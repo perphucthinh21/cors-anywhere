78a7d9f03157c7f2d292f45372710734c7dc40c4
perthinh <perthinh@gmail.com>
2021-12-09 09:37:20 +0700

    update

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const corsProxy = require('cors-anywhere');
+const corsProxy = require("cors-anywhere");
 
 // Listen on a specific host via the HOST environment variable.
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment variable.
 const port = process.env.PORT || 8080;
 
-corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'https://portfolio-ravenous.herokuapp.com', 'http://18.191.112.47', 'http://spm.cnexco.com', 'https://spm.cnexco.com', ''],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, () => {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+corsProxy
+	.createServer({
+		originWhitelist: [
+			"http://localhost:3000",
+			"https://portfolio-ravenous.herokuapp.com",
+			"http://18.191.112.47",
+			"http://spm.cnexco.com",
+			"https://spm.cnexco.com",
+			"http://123.25.30.61:195",
+			"",
+		],
+		removeHeaders: ["cookie", "cookie2"],
+	})
+	.listen(port, host, () => {
+		console.log("Running CORS Anywhere on " + host + ":" + port);
+	});
